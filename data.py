a4dc44f783e5daff73b922c93517620d543d7097
Raza Poonja <razapoonja@gmail.com>
2019-01-31 03:14:20 +0500

    Setting it up

---
+++ data.py
@@ -0,0 +1,17 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'One',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident necessitatibus, expedita, odit et quisquam consequatur, quis delectus, obcaecati commodi eveniet omnis repudiandae autem! Reiciendis tempora ut in, aut consequuntur autem.',
+			'author': 'John',
+			'create_date': '01-21-2019'
+		},
+		{
+			'id': 2,
+			'title': 'Two',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident necessitatibus, expedita, odit et quisquam consequatur, quis delectus, obcaecati commodi eveniet omnis repudiandae autem! Reiciendis tempora ut in, aut consequuntur autem.',
+			'author': 'Doe',
+			'create_date': '01-21-2019'
+		}
+	]
